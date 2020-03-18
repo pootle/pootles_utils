ef4d6d9cb6e8ddad4240cc5421ea64672765be87
pootle <iainmalcolm1@gmail.com>
2020-03-18 10:33:23 +0000

    updates to allow installation using setup

--- setup.py
+++ setup.py
@@ -1,22 +1,9 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pootle_utils-Pootle", # Replace with your own username
-    version="0.1",
-    author="Iain Malcolm",
-    author_email="iainmalcolm1@gmail.com",
-    description="stuff I use in various other packages",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pootle/pootles_utils",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Universal Permissive License (UPL)",
-        "Operating System :: POSIX :: Linux",
-    ],
-    python_requires='>=3.5',
-)
+setup(name='pootles_utils',
+      version='0.0.1',
+      description='some generally useful modules',
+      author='Iain Malcolm',
+      url='https://github/',
+      packages=['pootlestuff'],
+      )
